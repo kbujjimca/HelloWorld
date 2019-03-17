cc54410dcb5c510bcdb11be6d3069263792c7329
bujji <bujji@bujjis-mbp.hitronhub.home>
2019-03-16 20:10:28 -0600

    Helloworld

---
+++ src/com/src/test/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.src.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is hello world example");
+
+	}
+
+}
