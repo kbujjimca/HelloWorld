49cf46a568133ef72a082189b45e88bcb3f42616
bujji <bujji@bujjis-mbp.hitronhub.home>
2019-04-28 11:55:15 -0600

    Commit

--- src/com/src/test/HelloWorld.java
+++ src/com/src/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is hello world example15");
+		System.out.println("This is hello world example15.Build the pipeline in Jenkins");
 
 	}
 
